9da240ece168a94fa85836dcb00dbabfa2708fba
Fuzzy <rodrigo.rosa.005@acad.pucrs.br>
2019-10-14 11:06:29 -0300

    Global styles :nail_care:

--- src/global.js
+++ src/global.js
@@ -1,15 +1,18 @@
 import styled, { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     outline: 0;
 }
+
 html, body, #root {
     height: 100%;
 }
+
 body{
     @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,700&display=swap');
     text-rendering: optimizeLegibility;
